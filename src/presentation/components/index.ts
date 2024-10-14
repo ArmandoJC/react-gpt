66b44a798a2dea988b399f47df27a7e87a5aab28
Slater <armando.jaimes@pucp.edu.pe>
2024-10-14 15:29:40 -0500

    Obtener audio y reproducirlo

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,8 +1,12 @@
 export * from './sidebar/SideBarMenuItem'
+
 export * from './chat-bubbles/GptMessage'
-export * from './chat-bubbles/GptOrthographyMessage'
 export * from './chat-bubbles/MyMessage'
-export * from './loaders/TypingLoader'
+export * from './chat-bubbles/GptMessageAudio'
+export * from './chat-bubbles/GptOrthographyMessage'
+
 export * from './chat-input-boxes/TextMessageBox';
 export * from './chat-input-boxes/TextMessageBoxFile';
 export * from './chat-input-boxes/TextMessageBoxSelect';
+
+export * from './loaders/TypingLoader'
