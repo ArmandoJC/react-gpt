2170a1f792c9c58a39f3e3b1f0a195fc2bd3aee4
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-10-28 17:38:30 -0500

    Página de generación de imágenes

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,6 +1,80 @@
+import { useState } from 'react';
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from '../../components';
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+    info?: {
+        imageUrl: string;
+        alt: string;
+    }
+}
+
+
+
 
 export const ImageGenerationPage = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([])
+
+
+    const handlePost = async (text: string) => {
+
+        setIsLoading(true);
+        setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+        //TODO: UseCase
+
+        setIsLoading(false);
+
+        // Todo: Añadir el mensaje de isGPT en true
+
+
+    }
+
+
+
     return (
-        <div>ImageGenerationPage</div>
-    )
-}
+        <div className="chat-container">
+            <div className="chat-messages">
+                <div className="grid grid-cols-12 gap-y-2">
+                    {/* Bienvenida */}
+                    <GptMessage text="¿Qué imagen deseas generar hoy?" />
+
+                    {
+                        messages.map((message, index) => (
+                            message.isGpt
+                                ? (
+                                    <GptMessage key={index} text="Esto es de OpenAI" />
+                                )
+                                : (
+                                    <MyMessage key={index} text={message.text} />
+                                )
+
+                        ))
+                    }
+
+
+                    {
+                        isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )
+                    }
+
+
+                </div>
+            </div>
+
+
+            <TextMessageBox
+                onSendMessage={handlePost}
+                placeholder='Escribe aquí lo que deseas'
+                disableCorrections
+            />
+
+        </div>
+    );
+};
